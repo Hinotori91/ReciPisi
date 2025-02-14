2cc006e1c5e847c2937ae15afd173d90743991fd
Hinotori91 <petra.camondo@chello.at>
2025-02-14 15:18:42 +0100

    navbar buttons centered to logo

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-4 text-white">
-      <ul className="flex space-x-4">
+      <ul className="flex space-x-4 items-center">
         <li>
           <Link href="/"><Image src="/Logo/ChatGPT_Logo_Versuch.png" alt="Logo" width={70} height={100} />
           </Link>
