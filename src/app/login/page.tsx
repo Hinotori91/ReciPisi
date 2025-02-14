20a10c6fb40d72054e89db4f780d2c3bb62fc6c6
Hinotori91 <petra.camondo@chello.at>
2025-02-14 15:36:41 +0100

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+
+const LoginInput = () => {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-2 items-center">
+        <label htmlFor="mail">Mail</label>
+        <input id="mail" type="text" placeholder="mail" />
+      </div>
+      <div className="flex flex-col gap-2 items-center">
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" placeholder="password" />
+      </div>
+      <div className="flex justify-center">
+        <input type="submit" value="Login" className="text-center border-2 border-indigo-600 p-3 rounded-2xl w-72" />
+      </div>
+    </div>
+  )
+}
+
+// const Header = () => {
+//   return <h1>Login</h1>
+// }
+
+const RegistryButton = () => {
+  return <Link href="/registry" className="border-2 border-indigo-600 p-3 rounded-2xl w-72 text-center">Registry</Link>
+}
+
 export default function Login() {
-  return <h1>Login</h1>
+  return (
+    <div className="flex flex-col gap-5 items-center bg-sky-950 rounded-3xl w-80 p-10">
+      {/* <Header /> */}
+      <LoginInput />
+      or
+      <RegistryButton />
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Login from "./login/page"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Willkommen bei ReciPisi</h1>
-      <Image src="/Logo/ChatGPT_Logo_Versuch.png" alt="Logo" width={150} height={100} />
+    <div className="flex gap-5 items-center justify-center min-h-screen">
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold underline">Willkommen bei</h1>
+        <Image src="/Logo/ChatGPT_Logo_Versuch.png" alt="ResiPisi" width={250} height={200} />
+      </div>
+      <Login />
     </div>
   )
 }
