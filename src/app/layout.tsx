aaf9b0faa6bd974ae62da6307f2a75effd89e128
GalbavyD <dominik@galbavy.net>
2025-08-12 02:10:07 +0200

    inter is now used in page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
-import Navbar from './components/Navbar'
 import '@/app/globals.css'
+import localFont from 'next/font/local'
+
+const inter = localFont({
+	src: [
+		{
+			path: 'fonts/inter/web/Inter-Regular.woff2',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: 'fonts/inter/web/Inter-ExtraBold.woff2',
+			weight: '800',
+			style: 'extrabold'
+		}
+	],
+	display: 'swap'
+})
 
 export default function RootLayout({
 	children,
@@ -7,9 +23,11 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="de">
+		<html lang="de" className={inter.className}>
 			<body>
-				<Navbar />
+				<div className="flex space-between px-8 py-2">
+					<span className='text-2xl font-extrabold'>Recipisi</span>
+				</div>
 				{children}
 			</body>
 		</html>
