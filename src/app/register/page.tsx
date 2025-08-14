adc2e044f2522709d4d597f68e8b263fdefc438e
GalbavyD <dominik@galbavy.net>
2025-08-14 02:28:21 +0200

    removed commented out code

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,6 @@
 import Button from '@/components/Button'
 import Card from '@/components/Card'
 import LabeledInput from '@/components/LabeledInput'
-import Link from 'next/link'
 
 const Register = () => {
 	return (
@@ -18,35 +17,6 @@ const Register = () => {
 				</div>
 			</Card>
 		</div>
-		// <div className="flex flex-col gap-5 items-center bg-sky-950 rounded-3xl w-80 p-10">
-		// 	<div className="flex flex-col gap-2 items-center">
-		// 		<label htmlFor="username">Username</label>
-		// 		<input id="username" type="text" placeholder="username" />
-		// 	</div>
-		// 	<div className="flex flex-col gap-2 items-center">
-		// 		<label htmlFor="firstname">Firstname</label>
-		// 		<input id="firstname" type="text" placeholder="firstname" />
-		// 	</div>
-		// 	<div className="flex flex-col gap-2 items-center">
-		// 		<label htmlFor="lastname">Lastname</label>
-		// 		<input id="lastname" type="text" placeholder="lastname" />
-		// 	</div>
-		// 	<div className="flex flex-col gap-2 items-center">
-		// 		<label htmlFor="mail">Mail</label>
-		// 		<input id="mail" type="text" placeholder="mail" />
-		// 	</div>
-		// 	<div className="flex flex-col gap-2 items-center">
-		// 		<label htmlFor="password">Password</label>
-		// 		<input id="password" type="password" placeholder="password" />
-		// 	</div>
-		// 	<div className="flex justify-center">
-		// 		<input
-		// 			type="submit"
-		// 			value="Send"
-		// 			className="text-center border-2 border-indigo-600 p-3 rounded-2xl w-72"
-		// 		/>
-		// 	</div>
-		// </div>
 	)
 }
 
