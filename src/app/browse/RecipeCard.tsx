3674835b9744c608c63e6bf7ada3085f96b411c4
GalbavyD <dominik@galbavy.net>
2025-08-16 23:58:39 +0200

    added gap to grid and made recipe card take up whole space it is givenw

--- src/app/browse/RecipeCard.tsx
+++ src/app/browse/RecipeCard.tsx
@@ -3,7 +3,7 @@ import Card from '@/components/Card'
 type RecipeCardProps = { title?: string }
 
 const RecipeCard = ({ title = 'Unknown recipe' }: RecipeCardProps) => {
-	return <Card className="flex flex-col gap-2 p-2">{title}</Card>
+	return <Card className="flex flex-col gap-2 p-2 w-full">{title}</Card>
 }
 
 export default RecipeCard
--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -6,7 +6,7 @@ const Browse = () => {
 	return (
 		<div className="flex gap-2 h-full">
 			<Search></Search>
-			<div className="border flex-1 h-full grid grid-cols-3 overflow-scroll">
+			<div className="border flex-1 h-full grid grid-cols-3 overflow-scroll gap-2">
 				{_.times(20, (i) => {
 					return <RecipeCard title={`Recipe ${i}`}></RecipeCard>
 				})}
