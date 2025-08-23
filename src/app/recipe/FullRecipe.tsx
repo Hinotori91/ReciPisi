bd9ddb9f8266e362751eba0d4a62265ddd7915b3
GalbavyD <dominik@galbavy.net>
2025-08-23 02:15:10 +0200

    recipe page is now a bit more like the mockups

---
+++ src/app/recipe/FullRecipe.tsx
@@ -0,0 +1,38 @@
+import Card from '@/components/Card'
+import { Recipe } from '@/types/types'
+
+type FullRecipeProps = {
+	recipe: Recipe
+}
+
+const FullRecipe = ({ recipe }: FullRecipeProps) => {
+	return (
+		<div className="card-body gap-4 w-5xl">
+			<div className="flex gap-2 justify-between">
+				<span className="card-title overflow-scroll">{recipe.title}</span>
+				<div className="flex gap-2">
+					<button className="btn btn-primary">Start cooking mode</button>
+				</div>
+			</div>
+			<div className="flex gap-2 w-full">
+				{/* Left side */}
+				<div className="flex flex-1 flex-col gap-2 border-red-800 border">
+					<img />
+					<div>kochzeit&nutritions</div>
+					<div>Ingredients</div>
+				</div>
+				{/* Right side */}
+				<div className="flex flex-1 flex-col gap-2 border border-blue-800">
+					{recipe.steps.map((step, i) => (
+						<div>
+							<h3 className="text-lg">{`Step ${i + 1}`}</h3>
+							<div>{step}</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FullRecipe
--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,14 +1,31 @@
+import Card from '@/components/Card'
+import FullRecipe from './FullRecipe'
+
+import type { Recipe } from '@/types/types.ts'
+
 const Recipe = () => {
+	const r: Recipe = {
+		allergens: [],
+		energy: 345,
+		id: 'foo',
+		ingredients: [],
+		isFavourite: false,
+		nutritions: [],
+		steps: [
+			'Admire this page',
+			'Like, *really* admire this page',
+			'Tell Anders how fucking nice this page is',
+			'Admire the meal plan site for all its glory!',
+			'Be happy :D',
+		],
+		title: 'Dummy title',
+		totalTime: 3,
+	}
+
 	return (
-		<ul>
-			<li>Step 1: Admire this page</li>
-			<li>
-				Step 2: Like, <b>really</b> admire this page
-			</li>
-			<li>Step 3: Tell Anders how fucking nice this page is</li>
-			<li>Step 4: Admire the meal plan site for all its glory!</li>
-			<li>Step 5: Be happy :D</li>
-		</ul>
+		<Card className="m-auto">
+			<FullRecipe recipe={r} />
+		</Card>
 	)
 }
 
